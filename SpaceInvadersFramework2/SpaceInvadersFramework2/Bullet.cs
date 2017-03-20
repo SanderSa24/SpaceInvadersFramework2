9ed6a42efe142640afa7a6804325c373b2a45d6d
DESKTOP-QQ1IB8S\sande <s.serrano_altena@hotmail.com>
2017-03-20 22:28:47 +0100

    Stap 4: Create bullets an interaction with the invaders

---
+++ SpaceInvadersFramework2/SpaceInvadersFramework2/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace SpaceInvadersFramework2
+{
+    class Bullet: SpriteGameObject
+    {
+        public Bullet(): base("bullet") {
+
+            velocity.Y = -10;
+        }
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            this.position += velocity;
+        }
+    }
+}
--- SpaceInvadersFramework2/SpaceInvadersFramework2/PlayingState.cs
+++ SpaceInvadersFramework2/SpaceInvadersFramework2/PlayingState.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +10,11 @@ namespace SpaceInvadersFramework2
     class PlayingState : GameObjectList
     {
         Player player;
-        GameObjectList invaders;
+        GameObjectList invaders, bullets;
         public PlayingState() {
             player = new Player(this);
             invaders = new GameObjectList();
-
+            bullets = new GameObjectList();
             var xPos = SpaceInvaders.Screen.X / 2 - player.Width / 2;
             var yPos =  SpaceInvaders.Screen.Y - player.Width;
             player.Position = new Vector2(xPos, yPos);
@@ -30,10 +31,35 @@ namespace SpaceInvadersFramework2
             }
             Add(player);
             Add(invaders);
+           
+        }
+        public override void HandleInput(InputHelper inputHelper)
+        {
+            base.HandleInput(inputHelper);
+            if (inputHelper.KeyPressed(Keys.Space))
+            {
+                Bullet bullet = new Bullet();
+                bullet.Position = player.Position;
+                this.Add(bullet);
+                bullets.Add(bullet);
+            }
+
         }
         public override void Update(GameTime gameTime)
         {
             base.Update(gameTime);
+            foreach (Bullet b in bullets.Objects)
+            {
+                foreach (Invader i in invaders.Objects)
+                {
+                    if (b.CollidesWith(i) && b.Visible && i.Visible)
+                    {
+                        i.Visible = false;
+                        b.Visible = false;
+                      
+                    }
+                }
+            }
         }
     }
 }
